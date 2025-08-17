93073a3e59e803be00d5c03db9e12676cefeeefa
benga3 <banks3kaba@gmail.com>
2025-08-17 20:36:35 +0200

    deployment to aws

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
-import {Cocktail} from './shared/models/cocktail.model'
 import {CocktailService} from './shared/services/cocktail.service'
 
-import { from } from 'rxjs';
 import { HeaderComponent } from './header/header.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { FormGroup } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [CocktailService],
-  imports: [HeaderComponent, RouterOutlet, RouterLink]
+  imports: [HeaderComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'cocktails';
